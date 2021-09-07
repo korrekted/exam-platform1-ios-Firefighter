bfeebe5e08c495ef5858700dd409f46f0cf29995
Andrey Chernyshev <andrey.chernyshev@simbirsoft.com>
2021-09-07 21:49:56 +0300

    build 1

---
+++ Firefighter/Classes/Misc/Appearance.swift
@@ -0,0 +1,21 @@
+//
+//  Appearance.swift
+//  Ged
+//
+//  Created by Andrey Chernyshev on 23.04.2021.
+//
+
+import UIKit
+
+struct Appearance {
+    static let backgroundColor = UIColor(integralRed: 247, green: 249, blue: 255)
+    
+    static let mainColor = UIColor(integralRed: 27, green: 41, blue: 68)
+    static let mainColorAlpha = UIColor(integralRed: 27, green: 41, blue: 68, alpha: 0.3)
+    
+    static let takeFreeTestColor = UIColor(integralRed: 255, green: 89, blue: 67)
+    
+    static let progress1Color = UIColor(integralRed: 27, green: 41, blue: 68)
+    static let progress2Color = UIColor(integralRed: 255, green: 89, blue: 67)
+    static let progress3Color = UIColor(integralRed: 139, green: 169, blue: 204)
+}
