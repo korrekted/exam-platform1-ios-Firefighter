30803fe543e0eda4a2de0629d19092f387e60a1f
akonst17 <akonst17@gmail.com>
2022-04-14 16:48:33 +0300

    upgrade; build 6

--- Firefighter/Classes/Misc/Appearance.swift
+++ Firefighter/Classes/Misc/Appearance.swift
@@ -18,4 +18,8 @@ struct Appearance {
     static let progress1Color = UIColor(integralRed: 27, green: 41, blue: 68)
     static let progress2Color = UIColor(integralRed: 255, green: 89, blue: 67)
     static let progress3Color = UIColor(integralRed: 139, green: 169, blue: 204)
+    
+    static let successColor = UIColor(integralRed: 46, green: 190, blue: 161)
+    static let errorColor = UIColor(integralRed: 255, green: 115, blue: 99)
+    static let warningColor = UIColor(integralRed: 255, green: 155, blue: 38)
 }
